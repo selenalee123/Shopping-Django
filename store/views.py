2c4d6e168a638de7d6df3658d33e8dea612da8ba
kim ly <kimly@Kim-Ly-MacBookPro.local>
2022-06-10 15:34:48 -0400

    filter, pagination, setting

--- store/views.py
+++ store/views.py
@@ -1,39 +1,37 @@
 
 from django.shortcuts import get_object_or_404
 from django.db.models.aggregates import Count
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
-from rest_framework.decorators import api_view
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-
-
+from rest_framework.pagination import PageNumberPagination
 from rest_framework import status
 
+from store.pagination import DefaultPagination
+
+from .filters import ProductFilter
 from .models import Collection, OrderItem, Product, Review
 from .serializers import CollectionSerializer, ProductSeralizer, ReviewSerializer
 
 
 class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSeralizer
-
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        collection_id = self.request.query_params.get('collection_id', None)
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-        return queryset
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = ProductFilter
+    pagination_class = DefaultPagination
+    search_fields = ['title', 'description']
+    ordering_fields = ['last_update', 'unit_price']
 
     def get_serializer_context(self):
         return {'request': self.request}
-    
+
     def destroy(self, request, *args, **kwargs):
         if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error': "Product cannot be deleted because it is associated with an order item"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         return super().destroy(request, *args, **kwargs)
-    
-   
+
 
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(
